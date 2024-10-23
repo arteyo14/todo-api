3a3c1c895518274abbc6536d9b468b8242fcbe96
Yo.Pitchayakorn <98311957+arteyo14@users.noreply.github.com>
2024-10-23 22:29:29 +0700

    ปรับ คำ main

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/api", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+  res.send("API Server is running");
 });
 
 app.listen(port, () => {
