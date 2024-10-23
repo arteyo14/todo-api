7771c7cbfd0d1c71d5758f73dcf66cb5f5ba255f
Yo.Pitchayakorn <98311957+arteyo14@users.noreply.github.com>
2024-10-24 00:43:14 +0700

    [auth: login] - เพิ่ม logic check ค่าว่าง username & password

--- src/controllers/auth/post.ts
+++ src/controllers/auth/post.ts
@@ -11,6 +11,19 @@ export const login = async (req: Request, res: Response) => {
     const { username, password } = req.body;
     const validate: { [key: string]: string } = {};
 
+    if (!username || username.trim() === "") {
+      validate.username = "โปรดระบุ";
+    }
+
+    if (!password || password.trim() === "") {
+      validate.password = "โปรดระบุ";
+    }
+
+    if (Object.keys(validate).length > 0) {
+      res.status(400).json({ status: false, code: 400, error: validate });
+      return;
+    }
+
     const user = await prisma.user.findUnique({ where: { username } });
 
     if (!user) {
